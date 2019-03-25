27413d4745c138d3bc1e9a13f694fe30fd41ef06
Kayler Renslow <kaylerrenslow@gmail.com>
2019-03-25 15:06:56 -0500

    updated OrinocoLexer doc and added acceptIncludedReader

--- code/src/arma/orinocosqf/OrinocoLexer.java
+++ code/src/arma/orinocosqf/OrinocoLexer.java
@@ -3,15 +3,11 @@ package arma.orinocosqf;
 import org.jetbrains.annotations.NotNull;
 
 /**
- * A lexer that tokenizes text (into "words" or "tokens") and submits each token
- * to a {@link OrinocoLexerStream}. This lexer also has a cyclic dependency on a
- * preprocessor (in shape of a {@link OrinocoLexerStream}). Due to the fact that
- * each token may have a macro inside it, the lexer stores a set of macro's as a
- * reference to know when the preprocessor is needed. When a preprocessor is
- * needed for a token, it submits the token to
- * {@link OrinocoLexerStream#preProcessToken(char[], int, int)}. Subsequently,
- * the preprocessed result re-enters the lexer for re-lexing via
- * {@link #acceptPreProcessedText(CharSequence)}.
+ * A lexer that tokenizes text (into "words" or "tokens") and submits each token to a {@link OrinocoLexerStream}. This lexer also has a
+ * cyclic dependency on a preprocessor (in shape of a {@link OrinocoLexerStream}). Due to the fact that each token may have a macro inside
+ * it, the lexer stores a set of macro's as a reference to know when the preprocessor is needed. When a preprocessor is needed for a token,
+ * it submits the token to {@link OrinocoLexerStream#preProcessToken(char[], int, int)}. Subsequently, the preprocessed result re-enters the
+ * lexer for re-lexing via {@link #acceptPreProcessedText(CharSequence)}.
  *
  * Example 1: <pre>
  * #define ONE 1
@@ -20,8 +16,8 @@ import org.jetbrains.annotations.NotNull;
  *
  * // The lexer sees ASSIGN(hello), which matches a macro. It feeds "ASSIGN(hello)" to the preprocessor.
  * // The preprocessor then fully preprocesses ASSIGN(hello), thus yielding "hello = 1;".
- * // The lexer then receives that text via {@link #acceptPreProcessedText(CharSequence)}. The lexer lexes "hello", "=", "1", and ";" and submits
- * // them to the proper {@link OrinocoLexerStream} accept method that doesn't involve preprocessing.
+ * // The lexer then receives that text via {@link #acceptPreProcessedText(CharSequence)}. The lexer lexes "hello", "=", "1", and ";" and
+ * // submits them to the proper {@link OrinocoLexerStream} accept method that doesn't involve preprocessing.
  * </pre>
  *
  * @author K
@@ -32,11 +28,18 @@ public class OrinocoLexer {
 		return 0; //todo
 	}
 
-	private final OrinocoReader r;
+	/**
+	 * The current {@link OrinocoReader} instance
+	 */
+	private @NotNull OrinocoReader reader;
 	private final OrinocoLexerStream lexerStream;
+	/**
+	 * The offset of tokens after preprocessing
+	 */
+	private int preprocessedOffset;
 
 	public OrinocoLexer(@NotNull OrinocoReader r, @NotNull OrinocoLexerStream lexerStream) {
-		this.r = r;
+		this.reader = r;
 		this.lexerStream = lexerStream;
 		lexerStream.setLexer(this);
 	}
@@ -49,12 +52,21 @@ public class OrinocoLexer {
 	}
 
 	/**
-	 * Accepts partially or fully preprocessed text (see Example 1 in class level
-	 * doc) from the {@link OrinocoLexerStream}.
+	 * Accepts partially or fully preprocessed text (see Example 1 in class level doc) from the {@link OrinocoLexerStream}.
 	 *
 	 * @param text the preprocessed, untokenized text
 	 */
 	public void acceptPreProcessedText(@NotNull CharSequence text) {
+
+	}
+
+	/**
+	 * Pushes the current {@link OrinocoReader} on a stack and lexes this reader wholely before then popping the stack and continuing
+	 * lexing.
+	 *
+	 * @param reader the reader to immediately begin lexing
+	 */
+	public void acceptIncludedReader(@NotNull OrinocoReader reader) {
 
 	}
 }
