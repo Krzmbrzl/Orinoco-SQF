7cf580bf8ea8d2cbe75399dc0fcb80f34650ccd0
Raven <krzmbrzl@gmail.com>
2019-04-02 11:47:20 +0200

    Allowed for null-arguments in GlueSegment constructor

--- code/src/arma/orinocosqf/bodySegments/GlueSegment.java
+++ code/src/arma/orinocosqf/bodySegments/GlueSegment.java
@@ -15,7 +15,11 @@ public class GlueSegment extends BodySegment {
 	protected final BodySegment left;
 	protected final BodySegment right;
 
-	public GlueSegment(@NotNull BodySegment left, @NotNull BodySegment right) {
+	/**
+	 * @param left The segment to the left of this GlueSegment or <code>null</code> if there's none
+	 * @param right The segment to the right of this GlueSegment or <code>null</code> if there's none
+	 */
+	public GlueSegment(BodySegment left, BodySegment right) {
 		this.left = left;
 		this.right = right;
 	}
@@ -24,16 +28,24 @@ public class GlueSegment extends BodySegment {
 	public void setOwner(@NotNull PreProcessorMacro macro) {
 		super.setOwner(macro);
 
-		left.setOwner(macro);
-		right.setOwner(macro);
+		if (left != null) {
+			left.setOwner(macro);
+		}
+		if (right != null) {
+			right.setOwner(macro);
+		}
 	}
 
 	@NotNull
 	@Override
 	public CharSequence applyArguments(@NotNull List<CharSequence> args) {
 		StringBuilder sb = new StringBuilder();
-		sb.append(left.applyArguments(args));
-		sb.append(right.applyArguments(args));
+		if (left != null) {
+			sb.append(left.applyArguments(args));
+		}
+		if (right != null) {
+			sb.append(right.applyArguments(args));
+		}
 		return sb;
 	}
 
@@ -41,14 +53,18 @@ public class GlueSegment extends BodySegment {
 	@NotNull
 	public CharSequence toStringNoPreProcessing() {
 		StringBuilder sb = new StringBuilder();
-		sb.append(left.toStringNoPreProcessing());
+		if (left != null) {
+			sb.append(left.toStringNoPreProcessing());
+		}
 		sb.append("##");
-		sb.append(right.toStringNoPreProcessing());
+		if (right != null) {
+			sb.append(right.toStringNoPreProcessing());
+		}
 		return sb;
 	}
 
 	@Override
 	public String toString() {
-		return "GlueSegment{left=" + left + ",right=" + right + '}';
+		return "GlueSegment{left=" + (left != null ? left : "Null") + ",right=" + (right != null ? right : "Null") + '}';
 	}
 }
