154a550976577c24ce905d33b043650eebd9c387
Raven <krzmbrzl@gmail.com>
2019-04-20 17:21:54 +0200

    added class for static access of different problems

---
+++ code/src/arma/orinocosqf/problems/Problems.java
@@ -0,0 +1,14 @@
+package arma.orinocosqf.problems;
+
+/**
+ * A utility class holding an instance of every available {@link Problem} implementation
+ * @author Raven
+ *
+ */
+public class Problems {
+	public static final Error_Generic GENERIC = new Error_Generic();
+	public static final Error_Internal INTERNAL = new Error_Internal();
+	public static final Error_InvalidCharacter INVALID_CHARACTER = new Error_InvalidCharacter();
+	public static final Error_UnclosedParenthesis UNCLOSED_PARENTHESIS = new Error_UnclosedParenthesis();
+	public static final Error_UnclosedString UNCLOSED_STRING = new Error_UnclosedString();
+}
