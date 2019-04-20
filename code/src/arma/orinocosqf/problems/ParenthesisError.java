14c7c132cf9823bdf11a57268f56327eaf82da7f
Raven <krzmbrzl@gmail.com>
2019-04-20 17:22:22 +0200

    added base-class for parenthesis-errors

---
+++ code/src/arma/orinocosqf/problems/ParenthesisError.java
@@ -0,0 +1,11 @@
+package arma.orinocosqf.problems;
+
+/**
+ * Base class of all errors related to parenthesis
+ * 
+ * @author Raven
+ *
+ */
+public abstract class ParenthesisError extends Problem.Error {
+	
+}
