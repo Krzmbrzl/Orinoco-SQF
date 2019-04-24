cbe8f637b4ca244c159a07be49f6133ffe52031d
Kayler Renslow <kaylerrenslow@gmail.com>
2019-04-24 18:52:06 -0500

    lexer progress

--- code/src/arma/orinocosqf/OrinocoLexer.java
+++ code/src/arma/orinocosqf/OrinocoLexer.java
@@ -4,11 +4,10 @@ import org.jetbrains.annotations.NotNull;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
-import static arma.orinocosqf.ASCIITextHelper.toLowerCase;
+import static arma.orinocosqf.SQFTokenNodes.SQFRootTokenNode;
 
 /**
  * A lexer that tokenizes text (into "words" or "tokens") and submits each token to a {@link OrinocoLexerStream}. This lexer also has a
@@ -39,7 +38,7 @@ public class OrinocoLexer {
 	}
 
 	private final OrinocoCharStream ocs = new OrinocoCharStream();
-	private final RootTokenNode rootTokenNode = new RootTokenNode(this);
+	private final RootTokenNode rootTokenNode = new SQFRootTokenNode(this);
 	private final OrinocoLexerStream lexerStream;
 	private int originalOffset = 0;
 	private int originalLength = 0;
@@ -60,13 +59,6 @@ public class OrinocoLexer {
 	 * Starts the lexing process.
 	 */
 	public void start() {
-		rootTokenNode.children.add(new MultilineCommentTokenNode(this));
-		rootTokenNode.children.add(new SingleLineCommentTokenNode(this));
-		rootTokenNode.children.add(new WhitespaceTokenNode(this, true));
-		rootTokenNode.children.add(new PreProcessorCommandTokenNode(this));
-
-		rootTokenNode.children.trimToSize();
-
 		// todo :backtrack
 		/*
 			Instead of clearing currentToken, have a parameter that marks what parts have been used. if currentToken hasn't had
@@ -187,363 +179,12 @@ public class OrinocoLexer {
 		throw new UnsupportedOperationException("Get context not yet implemented!");
 	}
 
-	private static class PreProcessorCommandTokenNode extends TokenNode {
-		private final int STATE1_NEED_HASH = 0;
-		private final int STATE2_WORD = 1;
-		private final int STATE3_WHITESPACE = 2;
-		private final int STATE4_BODY = 3;
-		private final int STATE5_NEXT_LINE = 4; //used when \ is encountered in body
-		private final int STATE6_DONE = 5;
-		private int state = STATE1_NEED_HASH;
-		private final MultilineCommentTokenNode commentNode;
-		private boolean inComment = false;
-		private PreProcessorCommand command = null;
-		private final StringBuilder commandName = new StringBuilder();
-		private final List<Integer> tokenRanges = new ArrayList<>();
-		private int currentIndex;
-		private int endIndexOfLastComment;
-
-		public PreProcessorCommandTokenNode(@NotNull OrinocoLexer lexer) {
-			super(lexer);
-			commentNode = new MultilineCommentTokenNode(lexer);
-			reset();
-		}
-
-		@Override
-		public void accept(char c) {
-			// #word body
-			// or
-			// #word body \
-			// more body
-			switch (state) {
-				case STATE1_NEED_HASH: {
-					if (c == '#') {
-						state = STATE2_WORD;
-					}
-					break;
-				}
-				case STATE2_WORD: {
-					if (Character.isWhitespace(c)) {
-						state = STATE3_WHITESPACE;
-						for (PreProcessorCommand command : PreProcessorCommand.values()) {
-							if (commandName.length() != command.name().length()) {
-								continue;
-							}
-							boolean match = true;
-							for (int i = 0; i < commandName.length(); i++) {
-								char cn = toLowerCase(commandName.charAt(i));
-								if (cn != toLowerCase(command.name().charAt(i))) {
-									match = false;
-									break;
-								}
-							}
-							if (match) {
-								this.command = command;
-							} else {
-								reset();
-							}
-						}
-						break;
-					}
-					commandName.append(c);
-					break;
-				}
-				case STATE3_WHITESPACE: {
-					if (!Character.isWhitespace(c)) {
-						state = STATE4_BODY;
-					}
-					break;
-				}
-				case STATE4_BODY: {
-					commentNode.accept(c);
-					// todo handle single line comments
-					if (commentNode.isActive()) {
-						if (!inComment) {
-							if (endIndexOfLastComment == -1) {
-								tokenRanges.add(0);
-								tokenRanges.add(currentIndex);
-							} else {
-								tokenRanges.add(endIndexOfLastComment + 1);
-								tokenRanges.add(currentIndex);
-							}
-						}
-						inComment = true;
-						break;
-					}
-					if (inComment) {
-						if (commentNode.hasCompletedWork()) {
-							inComment = false;
-							endIndexOfLastComment = currentIndex;
-						}
-						break;
-					}
-
-					if (c == '\\') {
-						state = STATE5_NEXT_LINE;
-						break;
-					}
-					if (c == '\n') {
-						state = STATE6_DONE;
-					}
-
-					break;
-				}
-				case STATE5_NEXT_LINE: {
-					if (c == '\n') {
-						state = STATE4_BODY;
-					}
-					break;
-				}
-			}
-			currentIndex++;
-		}
-
-		@Override
-		public boolean isActive() {
-			return state > STATE1_NEED_HASH && state < STATE6_DONE;
-		}
-
-		@Override
-		public boolean hasCompletedWork() {
-			return state == STATE6_DONE;
-		}
-
-		@Override
-		public void submitWork() {
-			if (state != STATE6_DONE) {
-				throw new IllegalStateException();
-			}
-			if (command == null) {
-				throw new IllegalStateException();
-			}
-			if (tokenRanges.size() == 0) {
-				tokenRanges.add(0);
-				tokenRanges.add(currentIndex);
-			} else if (tokenRanges.size() % 2 != 0) {
-				tokenRanges.add(currentIndex);
-			}
-			lexer.makePreProcessorCommand(command, tokenRanges);
-			reset();
-		}
-
-		@Override
-		public void reset() {
-			state = STATE1_NEED_HASH;
-			commandName.setLength(0);
-			command = null;
-			currentIndex = 0;
-			tokenRanges.clear();
-			endIndexOfLastComment = -1;
-		}
-
-		@Override
-		public void errorIncompleteState() {
-			// todo
-		}
+	public OrinocoLexerStream getLexerStream() {
+		return lexerStream;
 	}
 
-	private static class WhitespaceTokenNode extends TokenNode {
-		private final boolean makeWhitespaceToken;
-		private boolean working = false;
-		private boolean done = false;
 
-		public WhitespaceTokenNode(@NotNull OrinocoLexer lexer, boolean makeWhitespaceToken) {
-			super(lexer);
-			this.makeWhitespaceToken = makeWhitespaceToken;
-		}
-
-		@Override
-		public void accept(char c) {
-			if (Character.isWhitespace(c)) {
-				working = true;
-			} else {
-				if (working) {
-					working = false;
-					done = true;
-				}
-			}
-		}
-
-		@Override
-		public boolean isActive() {
-			return working;
-		}
-
-		@Override
-		public boolean hasCompletedWork() {
-			return done;
-		}
-
-		@Override
-		public void submitWork() {
-			if (!done) {
-				throw new IllegalStateException();
-			}
-			if (makeWhitespaceToken) {
-				lexer.makeWhitespace();
-			}
-			reset();
-		}
-
-		@Override
-		public void reset() {
-			done = false;
-		}
-
-		@Override
-		public void errorIncompleteState() {
-			// do nothing because there is no incomplete state for this
-		}
-	}
-
-	private static class SingleLineCommentTokenNode extends TokenNode {
-
-		private final int STATE1_NEED_SLASH = 0;
-		private final int STATE2_NEED_SECOND_SLASH = 1;
-		private final int STATE3_NEED_NEWLINE = 2;
-		private final int STATE4_MADE_COMMENT = 3;
-
-		private int state = STATE1_NEED_SLASH;
-
-		public SingleLineCommentTokenNode(@NotNull OrinocoLexer lexer) {
-			super(lexer);
-		}
-
-		@Override
-		public void accept(char c) {
-			switch (state) {
-				case STATE1_NEED_SLASH: {
-					if (c == '/') {
-						state = STATE2_NEED_SECOND_SLASH;
-					}
-					break;
-				}
-				case STATE2_NEED_SECOND_SLASH: {
-					if (c == '/') {
-						state = STATE3_NEED_NEWLINE;
-					} else {
-						reset();
-					}
-					break;
-				}
-				case STATE3_NEED_NEWLINE: {
-					if (c == '\n') {
-						state = STATE4_MADE_COMMENT;
-					}
-					break;
-				}
-			}
-		}
-
-		@Override
-		public boolean isActive() {
-			return state > STATE1_NEED_SLASH && state < STATE4_MADE_COMMENT;
-		}
-
-		@Override
-		public boolean hasCompletedWork() {
-			return state == STATE4_MADE_COMMENT;
-		}
-
-		@Override
-		public void submitWork() {
-			if (state != STATE4_MADE_COMMENT) {
-				throw new IllegalStateException();
-			}
-			reset();
-			this.lexer.makeComment();
-		}
-
-		@Override
-		public void reset() {
-			state = STATE1_NEED_SLASH;
-		}
-
-		@Override
-		public void errorIncompleteState() {
-			// todo tell lexer what is missing to complete a comment
-		}
-	}
-
-	private static class MultilineCommentTokenNode extends TokenNode {
-
-		private final int STATE1_NEED_SLASH = 0;
-		private final int STATE2_NEED_STAR = 1;
-		private final int STATE3_NEED_SECOND_STAR = 2;
-		private final int STATE4_NEED_SECOND_SLASH = 3;
-		private final int STATE5_MADE_COMMENT = 4;
-
-		private int state = STATE1_NEED_SLASH;
-
-		public MultilineCommentTokenNode(@NotNull OrinocoLexer lexer) {
-			super(lexer);
-		}
-
-		@Override
-		public void accept(char c) {
-			switch (state) {
-				case STATE1_NEED_SLASH: {
-					if (c == '/') {
-						state = STATE2_NEED_STAR;
-					}
-					break;
-				}
-				case STATE2_NEED_STAR: {
-					if (c == '*') {
-						state = STATE3_NEED_SECOND_STAR;
-					} else {
-						state = STATE1_NEED_SLASH;
-					}
-					break;
-				}
-				case STATE3_NEED_SECOND_STAR: {
-					if (c == '*') {
-						state = STATE4_NEED_SECOND_SLASH;
-					}
-					break;
-				}
-				case STATE4_NEED_SECOND_SLASH: {
-					if (c == '/') {
-						state = STATE5_MADE_COMMENT;
-					} else {
-						state = STATE1_NEED_SLASH;
-					}
-					break;
-				}
-			}
-		}
-
-		@Override
-		public boolean hasCompletedWork() {
-			return state == STATE5_MADE_COMMENT;
-		}
-
-		@Override
-		public void submitWork() {
-			if (state != STATE5_MADE_COMMENT) {
-				throw new IllegalStateException();
-			}
-			reset();
-		}
-
-		@Override
-		public void reset() {
-			state = STATE1_NEED_SLASH;
-			this.lexer.makeComment();
-		}
-
-		@Override
-		public void errorIncompleteState() {
-			// todo tell lexer what is missing to complete a comment
-		}
-
-		@Override
-		public boolean isActive() {
-			return state > STATE1_NEED_SLASH && state < STATE5_MADE_COMMENT;
-		}
-	}
-
-	private abstract static class TokenNode {
+	public abstract static class TokenNode {
 
 		protected final OrinocoLexer lexer;
 
@@ -576,92 +217,49 @@ public class OrinocoLexer {
 		 */
 		public abstract void reset();
 
-	}
 
-	private static class RootTokenNode extends TokenNode {
+		protected void makeWhitespace() {
+			lexer.makeWhitespace();
+		}
+
+		protected void makeComment() {
+			lexer.makeComment();
+		}
+
+		protected void makeLocalVariable() {
+			lexer.makeLocalVariable();
+		}
+
+		protected void makeGlobalVariable() {
+			lexer.makeGlobalVariable();
+		}
+
+		protected void makeCommand() {
+			lexer.makeCommand();
+		}
+
+		protected void makePreProcessorCommand(@NotNull PreProcessorCommand command, @NotNull List<Integer> tokenRanges) {
+			lexer.makePreProcessorCommand(command, tokenRanges);
+		}
+
+		protected void makePreProcessedText() {
+			lexer.makePreProcessedText();
+		}
 
 		@NotNull
-		public final ArrayList<TokenNode> children = new ArrayList<>();
-		private boolean childMatched = false;
+		protected CharSequence getCurrentToken() {
+			return lexer.currentToken;
+		}
+
+	}
+
+	public static abstract class RootTokenNode extends TokenNode {
 
 		public RootTokenNode(@NotNull OrinocoLexer lexer) {
 			super(lexer);
 		}
 
-		@Override
-		public void accept(char c) {
-			TokenNode finished = null;
-
-			int activeCount = 0;
-			for (TokenNode child : children) {
-				child.accept(c);
-				if (child.isActive()) {
-					activeCount++;
-				}
-				if (child.hasCompletedWork()) {
-					childMatched = true;
-					finished = child;
-				}
-			}
-			if (activeCount == 0 && finished != null) {
-				finished.submitWork();
-			}
-		}
-
-		@Override
-		public boolean isActive() {
-			for (TokenNode child : children) {
-				if (child.isActive()) {
-					return true;
-				}
-			}
-			return false;
-		}
-
-		@Override
-		public boolean hasCompletedWork() {
-			return childMatched && !isActive();
-		}
-
-		@Override
-		public void submitWork() {
-			if (!childMatched) {
-				throw new IllegalStateException();
-			}
-			reset();
-		}
-
-		@Override
-		public void reset() {
-			childMatched = false;
-		}
-
-		@Override
-		public void errorIncompleteState() {
-			for (TokenNode child : children) {
-				if (child.isActive()) {
-					child.errorIncompleteState();
-					break;
-				}
-			}
-		}
-
-		public void noMoreTokens() {
-			if (!isActive()) {
-				return;
-			}
-			boolean didSomething = false;
-			for (TokenNode child : children) {
-				if (child.hasCompletedWork()) {
-					child.submitWork();
-					didSomething = true;
-					break;
-				}
-			}
-			if (!didSomething) {
-				errorIncompleteState();
-			}
-		}
+		public abstract void noMoreTokens();
 	}
 
 	private static class OrinocoCharStream {
