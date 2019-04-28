eae478fd8414f26088f93fa3438f072c8b4c8b82
Raven <krzmbrzl@gmail.com>
2019-04-28 12:21:06 +0200

    added new problem

--- code/src/arma/orinocosqf/problems/Problems.java
+++ code/src/arma/orinocosqf/problems/Problems.java
@@ -6,10 +6,12 @@ package arma.orinocosqf.problems;
  *
  */
 public class Problems {
-	public static final Error_EmptyInput EMPTY = new Error_EmptyInput();
-	public static final Error_Generic GENERIC = new Error_Generic();
-	public static final Error_Internal INTERNAL = new Error_Internal();
-	public static final Error_InvalidCharacter INVALID_CHARACTER = new Error_InvalidCharacter();
-	public static final Error_UnclosedParenthesis UNCLOSED_PARENTHESIS = new Error_UnclosedParenthesis();
-	public static final Error_UnclosedString UNCLOSED_STRING = new Error_UnclosedString();
+	public static final Error_EmptyInput ERROR_EMPTY = new Error_EmptyInput();
+	public static final Error_Generic ERROR_GENERIC = new Error_Generic();
+	public static final Error_Internal ERROR_INTERNAL = new Error_Internal();
+	public static final Error_InvalidCharacter ERROR_INVALID_CHARACTER = new Error_InvalidCharacter();
+	public static final Error_UnclosedParenthesis ERROR_UNCLOSED_PARENTHESIS = new Error_UnclosedParenthesis();
+	public static final Error_UnclosedString ERROR_UNCLOSED_STRING = new Error_UnclosedString();
+	
+	public static final Warning_OverwriteExisting WARNING_OVERWRITE = new Warning_OverwriteExisting();
 }
