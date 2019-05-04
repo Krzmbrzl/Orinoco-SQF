86e978bc65b3ee72e7a8bc85024864d7fcd3b49a
Raven <krzmbrzl@gmail.com>
2019-05-04 12:06:06 +0200

    Improved absolute path handling (especeially PBO prefixes)

--- code/src/arma/orinocosqf/ArmaFilesystem.java
+++ code/src/arma/orinocosqf/ArmaFilesystem.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
@@ -20,6 +21,41 @@ import arma.orinocosqf.exceptions.InvalidPathException;
  */
 public class ArmaFilesystem {
 	/**
+	 * A helper class for resolving absolute paths agains PBO-prefixes
+	 * 
+	 * @author Raven
+	 *
+	 */
+	static class PrefixPath {
+		/**
+		 * The PBO prefix
+		 */
+		public Path prefix;
+		/**
+		 * The path that is to be set equivalent to the PBO prefix
+		 */
+		public Path prefixLocation;
+
+		public PrefixPath(@NotNull Path prefix, @NotNull Path prefixLocation) {
+			this.prefix = prefix;
+			this.prefixLocation = prefixLocation;
+		}
+
+		/**
+		 * @param currentPath The path currently searched for (potentially starting with the given prefix)
+		 * @return The path in the actual FileSystem corresponding to the provided path. This resolution is based on {@link #prefix} and
+		 *         {@link #prefixLocation}
+		 */
+		public Path prefixPath(@NotNull Path currentPath) {
+			if (currentPath.startsWith(prefix)) {
+				return prefixLocation.resolve(prefix.relativize(currentPath));
+			} else {
+				return currentPath;
+			}
+		}
+	}
+
+	/**
 	 * The paths to search for resources when resolving an absolute path
 	 */
 	protected List<File> searchPaths;
@@ -32,6 +68,12 @@ public class ArmaFilesystem {
 	public ArmaFilesystem(@NotNull Path cwd, @NotNull List<File> searchPaths) {
 		this.cwd = cwd;
 		this.searchPaths = searchPaths;
+
+		// Add CWD to search path if it isn't included anyways
+		File cwdFile = cwd.toFile();
+		if (!searchPaths.contains(cwdFile)) {
+			searchPaths.add(cwdFile);
+		}
 	}
 
 	/**
@@ -62,13 +104,14 @@ public class ArmaFilesystem {
 		if (path.charAt(0) == separator) {
 			// absolute path
 			for (File currentSearchDir : searchPaths) {
-				ArmaFile target = createArmaFile(resolveAbsolute(currentSearchDir, path), path);
+				ArmaFile target = createArmaFile(resolveAbsolute(currentSearchDir, path, null), path);
 
 				if (target != null) {
 					return target;
 				}
 			}
-			throw new InvalidPathException("Absolute path handling is not yet implemented!");
+
+			return null;
 		} else {
 			// relative path -> can be treated as a normal file-path
 			File includeFile = cwd.resolve(path).toFile();
@@ -107,50 +150,68 @@ public class ArmaFilesystem {
 	 * 
 	 * @param currentFile The current file to search
 	 * @param path The path to search for
+	 * @param prefixPath The {@link PrefixPath} that is used to describe the current directories once a PBO prefix has been determined. If
+	 *        no prefix has been encountered (yet), this should be <code>null</code>.
 	 * @return A {@link File} corresponding to the given path or <code>null</code> if the path couldn't be found
 	 */
 	@Nullable
-	protected File resolveAbsolute(@NotNull File currentFile, @NotNull String path) {
+	protected File resolveAbsolute(@NotNull File currentFile, @NotNull String path, @Nullable PrefixPath prefixPath) {
+		if (prefixPath != null) {
+			// Try to match against found prefix
+			if (!path.startsWith(prefixPath.prefix.toString())) {
+				// This directory is labeled with this prefix and it doesn't match the searched path -> the search won't succeed in this
+				// directory
+				return null;
+			}
+
+			File target = prefixPath.prefixPath(Paths.get(path)).toFile();
+
+			if (target.exists()) {
+				return target;
+			} else {
+				// This search won't succeed in this directory
+				return null;
+			}
+		} else {
+			// try to match against existing file structure
+			if (path.startsWith(File.separator + currentFile.getName())) {
+				File target = new File(currentFile.getParentFile(), path.substring(1)); // substring to remove leading separator
+
+				if (target.exists()) {
+					return target;
+				}
+			}
+		}
+
 		if (currentFile.isDirectory()) {
 			for (File currentChild : currentFile.listFiles()) {
-				if (path.startsWith(File.separator + currentChild.getName())) {
-					// try to match against existing file structure
-					File target = new File(currentFile, path.substring(1)); // substring to remove leading separator
-
-					if (target.exists()) {
-						return target;
-					}
-				}
-
-				File found = resolveAbsolute(currentChild, path);
+				File found = resolveAbsolute(currentChild, path, prefixPath);
 				if (found != null) {
 					return found;
 				}
 			}
 		} else {
 			if (currentFile.isFile()) {
-				if (currentFile.getName().toLowerCase().toLowerCase().contains("pboprefix") && currentFile.canRead()
+				if (prefixPath == null && currentFile.getName().toLowerCase().toLowerCase().contains("pboprefix") && currentFile.canRead()
 						&& currentFile.length() <= 5000) {
 					// Read the prefix and check against path but don't even consider files greater 5kB
 					try {
-						String content = new String(Files.readAllBytes(currentFile.toPath()), Charset.defaultCharset()).replace('\\',
+						String prefix = new String(Files.readAllBytes(currentFile.toPath()), Charset.defaultCharset()).replace('\\',
 								File.separatorChar);
-						if (content.contains("\n")) {
-							content = content.substring(0, content.indexOf("\n")).trim();
+						if (prefix.contains("\n")) {
+							prefix = prefix.substring(0, prefix.indexOf("\n")).trim();
 						}
-						if (!content.startsWith(File.separator)) {
-							content = File.separator + content;
+						if (!prefix.startsWith(File.separator)) {
+							prefix = File.separator + prefix;
 						}
-						if (!content.endsWith(File.separator)) {
-							content = content + File.separator;
+						if (!prefix.endsWith(File.separator)) {
+							prefix = prefix + File.separator;
 						}
 
-						if (path.startsWith(content)) {
+						if (path.startsWith(prefix)) {
 							// This directory is to be interpreted as if it was reachable under the path specified in the prefix file
-							// adjust path to point to this directory and start searching this directory again
-							path = path.substring(content.length());
-
-							resolveAbsolute(currentFile, path);
+							return resolveAbsolute(currentFile.getParentFile(), path,
+									new PrefixPath(Paths.get(prefix), currentFile.getParentFile().toPath()));
 						}
 					} catch (IOException e) {
 						e.printStackTrace();
