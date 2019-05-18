765c27fd4d7d3e83c225b63043acef50a9821d43
Raven <krzmbrzl@gmail.com>
2019-05-18 15:37:21 +0200

    progress on expansion tests

--- code/tests/arma/orinocosqf/BodySegmentExpansionTest.java
+++ code/tests/arma/orinocosqf/BodySegmentExpansionTest.java
@@ -6,16 +6,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.jetbrains.annotations.NotNull;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
+import arma.orinocosqf.exceptions.MissingMacroArgumentException;
+import arma.orinocosqf.exceptions.NoMacroArgumentsGivenException;
+import arma.orinocosqf.exceptions.OrinocoPreprocessorException;
 import arma.orinocosqf.helpers.NoProblemListener;
 import arma.orinocosqf.helpers.TestOrinocoPreprocessor;
-import arma.orinocosqf.preprocessing.MacroSet;
+import arma.orinocosqf.preprocessing.PreProcessorCommand;
 import arma.orinocosqf.preprocessing.PreProcessorMacro;
 import arma.orinocosqf.preprocessing.bodySegments.BodySegment;
 import arma.orinocosqf.preprocessing.bodySegments.BodySegmentParser;
+import arma.orinocosqf.preprocessing.bodySegments.BodySegmentSequence;
+import arma.orinocosqf.preprocessing.bodySegments.ParenSegment;
 import arma.orinocosqf.preprocessing.bodySegments.TextSegment;
+import arma.orinocosqf.preprocessing.bodySegments.WordSegment;
 
 public class BodySegmentExpansionTest {
 
@@ -23,7 +30,6 @@ public class BodySegmentExpansionTest {
 	static TestOrinocoPreprocessor p;
 	static List<String> macroArguments;
 	static PreProcessorMacro pseudoMacro;
-	static MacroSet macroSet;
 	final static List<CharSequence> NO_ARGS = new ArrayList<>(0);
 
 	@BeforeClass
@@ -31,41 +37,137 @@ public class BodySegmentExpansionTest {
 		segmentParser = new BodySegmentParser(new NoProblemListener());
 		p = new TestOrinocoPreprocessor();
 		macroArguments = new ArrayList<>();
-		macroSet = new MacroSet();
-		pseudoMacro = new PreProcessorMacro(macroSet, "NonExistantMacro", new ArrayList<>(), new TextSegment("I don't really exist"));
-	}
-
-	void assertSegmentNoMacro(@NotNull String input) {
-		assertSegment(input, input, NO_ARGS);
+		pseudoMacro = new PreProcessorMacro(p.getMacroSet(), "NonExistantMacro", new ArrayList<>(),
+				new TextSegment("I don't really exist"));
 	}
 	
-	void assertSegment(@NotNull String originalInput, @NotNull String preprocessedInput) {
+	@Before
+	public void clearMacros() {
+		p.getMacroSet().clear();
+	}
+
+	void assertSegmentNoMacro(@NotNull String input) throws OrinocoPreprocessorException {
+		assertSegment(input, input, NO_ARGS);
+	}
+
+	void assertSegment(@NotNull String originalInput, @NotNull String preprocessedInput) throws OrinocoPreprocessorException {
 		assertSegment(originalInput, preprocessedInput, NO_ARGS);
 	}
 
-	void assertSegment(@NotNull String originalInput, @NotNull String preprocessedInput, @NotNull List<CharSequence> args) {
+	void assertSegment(@NotNull String originalInput, @NotNull String preprocessedInput, @NotNull List<CharSequence> args)
+			throws OrinocoPreprocessorException {
 		BodySegment segment = segmentParser.parseSegments(originalInput.toCharArray(), 0, originalInput.length(), macroArguments,
 				(c, isFirstLetter) -> p.isMacroNamePart(c, isFirstLetter));
 
 		segment.setOwner(pseudoMacro);
 
-		assertEquals("Expected unpreprocessed input to be equal to original input", originalInput, segment.toStringNoPreProcessing().toString());
+		assertEquals("Expected unpreprocessed input to be equal to original input", originalInput,
+				segment.toStringNoPreProcessing().toString());
 		assertEquals("Preprocessed to different result", preprocessedInput, segment.applyArguments(args).toString());
 	}
 
+	void defineMacro(@NotNull String definition) {
+		if (!definition.startsWith("#define ")) {
+			definition = "#define " + definition;
+		}
+
+		p.acceptPreProcessorCommand(PreProcessorCommand.Define, definition.toCharArray(), 0, definition.length());
+	}
+
 	@Test
-	public void no_macros() {
+	public void no_macros() throws OrinocoPreprocessorException {
 		assertSegmentNoMacro("test");
 		assertSegmentNoMacro("I can do what (i, want)");
 	}
-	
+
 	@Test
-	public void simpleExpansion() {
-		macroSet.put("MACRO", new PreProcessorMacro(macroSet, "MACRO", new ArrayList<>(), new TextSegment("Test")));
-		
+	public void simpleExpansion() throws OrinocoPreprocessorException {
+		defineMacro("#define MACRO Test");
+
 		assertSegment("Hello MACRO", "Hello Test");
-		
+
 		assertSegment("Hello (MACRO)", "Hello (Test)");
+	}
+
+	@Test
+	public void expansion_with_arguments() throws OrinocoPreprocessorException {
+		defineMacro("#define MACRO(a) Hello a");
+		defineMacro("#define MACRO2(a,b) Hello a and b");
+		defineMacro("#define MACRO3(a,b,c) |a|b|c|");
+
+		assertSegment("MACRO(world)", "Hello world");
+		assertSegment("MACRO( world)", "Hello  world");
+		assertSegment("MACRO( world )", "Hello  world ");
+		
+		assertSegment("MACRO2(world,)", "Hello world and ");
+		assertSegment("MACRO2(,more)", "Hello  and more");
+		assertSegment("MACRO2(,)", "Hello  and ");
+		assertSegment("MACRO2(world,more)", "Hello world and more");
+		assertSegment("MACRO2(world, more)", "Hello world and  more");
+		assertSegment("MACRO2(world, more )", "Hello world and  more ");
+		
+		assertSegment("MACRO3(a,test,here)", "|a|test|here|");
+	}
+
+	@Test
+	public void expectedExceptions() throws OrinocoPreprocessorException {
+		defineMacro("#define MACRO(a) Hello a");
+		boolean caughtException = false;
+
+		// expanding the WordSegment directly should yield a MissingMacroArgumentException
+		// because MACRO needs an argument a to be given
+		try {
+			WordSegment seg = new WordSegment("MACRO");
+			seg.setOwner(pseudoMacro);
+
+			seg.applyArguments(NO_ARGS);
+		} catch (MissingMacroArgumentException e) {
+			caughtException = true;
+		}
+
+		assertTrue("Expected an instance of MissingMacroArgumentException to be thrown", caughtException);
+
+		caughtException = false;
+
+
+		// expanding the WordSegment in a BodySegmentSequence should yield a NoMacroArgumentsGivenException
+		// because MACRO needs arguments
+		try {
+			List<BodySegment> segments = new ArrayList<>(2);
+			WordSegment seg = new WordSegment("MACRO");
+			seg.setOwner(pseudoMacro);
+			segments.add(seg);
+
+			BodySegmentSequence sequence = new BodySegmentSequence(segments);
+
+			sequence.applyArguments(NO_ARGS);
+		} catch (NoMacroArgumentsGivenException e) {
+			caughtException = true;
+		}
+
+		assertTrue("Expected an instance of NoMacroArgumentsGivenException to be thrown", caughtException);
+
+		caughtException = false;
+		
+		// expanding the WordSegment in a BodySegmentSequence should yield a MissingMacroArgumentException
+		// because MACRO needs one argument but is only presented with an empty argument block
+		try {
+			List<BodySegment> segments = new ArrayList<>(2);
+			WordSegment seg = new WordSegment("MACRO");
+			seg.setOwner(pseudoMacro);
+			segments.add(seg);
+			segments.add(new ParenSegment(new ArrayList<>()));
+
+			BodySegmentSequence sequence = new BodySegmentSequence(segments);
+
+			sequence.applyArguments(NO_ARGS);
+		} catch (MissingMacroArgumentException e) {
+			caughtException = true;
+		}
+
+		assertTrue("Expected an instance of MissingMacroArgumentException to be thrown", caughtException);
+
+		caughtException = false;
 	}
 
 }
