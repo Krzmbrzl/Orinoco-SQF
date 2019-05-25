4d5ba05ec7d5c5b0f3a9679ce94581f7512ca9bb
Raven <krzmbrzl@gmail.com>
2019-05-25 09:19:29 +0200

    Added TestSuite that allows to run all tests at once

---
+++ code/tests/arma/orinocosqf/OrinocoTestSuite.java
@@ -0,0 +1,12 @@
+package arma.orinocosqf;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ArmaFilesystemTest.class, ArrayValueHolderTest.class, BodySegmentExpansionTest.class, BodySegmentParserTest.class,
+		OrinocoLexerTest.class, OrinocoPreProcessorTest.class, ProblemImplementationTest.class, ValueTypeEquivalenceTest.class })
+public class OrinocoTestSuite {
+
+}
