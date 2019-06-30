3a362af06fb442f1c9bc83e9881ee86350cdad2c
Raven <krzmbrzl@gmail.com>
2019-06-30 13:54:18 +0200

    added a method for getting all buffered text; added description to append method

--- code/src/arma/orinocosqf/TextBuffer.java
+++ code/src/arma/orinocosqf/TextBuffer.java
@@ -17,16 +17,31 @@ public interface TextBuffer extends CharSequence, Appendable {
 	String getText(int offset, int length) throws IndexOutOfBoundsException;
 
 	/**
+	 * @return All text buffered in this object
+	 */
+	@NotNull
+	String getText();
+
+	/**
 	 * Gets text from this buffer and writes it into the given char-buffer
 	 *
-	 * @param buffer The buffer to write into (must at least be of length <code>boffset + length</code>)
+	 * @param buffer The buffer to write into (must at least be of length <code>offset + length</code>)
 	 * @param boffset The offset inside the provided buffer at which the first retrieved character is written
 	 * @param offset The offset of the first character of the text that is to be extracted
 	 * @param length The length of the text that is to be extracted
 	 * @throws IndexOutOfBoundsException When the requested text-area is not contained in this buffer or if the provided char[]-buffer is
-	 * not big enough to hold all of the requested text
+	 *         not big enough to hold all of the requested text
 	 */
 	void getText(char[] buffer, int boffset, int offset, int length) throws IndexOutOfBoundsException;
 
+	/**
+	 * Appends text from the given buffer to this object
+	 * 
+	 * @param buffer The buffer to read the text from
+	 * @param offset The start.offset from which on text should be transferred to this object
+	 * @param length The length of the text area to be transferred
+	 * @throws IndexOutOfBoundsException
+	 * @throws IOException
+	 */
 	void append(char[] buffer, int offset, int length) throws IndexOutOfBoundsException, IOException;
 }
